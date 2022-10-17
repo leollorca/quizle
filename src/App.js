14af629a9b64422a8777e122b30624c13ca9f7d5
Léo Llorca <leollorca@macbook-air-de-leo.home>
2022-10-17 15:51:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Test from "./components/Test";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Welcome to Quizzle!</h1>
+      <Test title="Test" />
+    </main>
+  );
+}
---
+++ src/components/Test.js
@@ -0,0 +1,3 @@
+export default function Test(props) {
+  return <h1>{props.title}</h1>;
+}
