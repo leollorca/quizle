09ed319f51283df2c9d0a6faa180e4778c664760
Léo Llorca <leollorca@macbook-air-de-leo.home>
2022-10-17 18:55:10 +0200

    adds the choice of difficulty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import Test from "./components/Test";
+import { useState } from "react";
+import Quiz from "./components/Quiz";
 
 export default function App() {
+  const [difficulty, setDifficulty] = useState(undefined);
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
+
+  function startQuiz() {
+    setIsQuizStarted(true);
+  }
+
+  function handleDifficulty(e) {
+    e.preventDefault();
+    setDifficulty(e.target.value);
+    startQuiz();
+  }
+
   return (
     <main>
-      <h1>Welcome to Quizzle!</h1>
-      <Test title="Test" />
+      {isQuizStarted ? (
+        <Quiz difficulty={difficulty} />
+      ) : (
+        <>
+          <h1>Let's start a quiz!</h1>
+          <input type="submit" value="easy" onClick={handleDifficulty} />
+          <input type="submit" value="medium" onClick={handleDifficulty} />
+          <input type="submit" value="hard" onClick={handleDifficulty} />
+        </>
+      )}
     </main>
   );
 }
