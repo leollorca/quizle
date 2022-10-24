083d071844daa6424ed733c3b6f19983f718a0b3
Léo Llorca <leollorca@macbook-air-de-leo.home>
2022-10-24 20:43:27 +0200

    check if quiz is filled before submit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,20 @@
-import { useState } from "react";
 import Answer from "./Answer";
 
 export default function Question(props) {
-  const [answers, setAnswers] = useState(props.answers);
-
-  function selectAnswer(event) {
-    setAnswers((prevAnswers) => {
-      return prevAnswers.map((prevAnswer) => {
-        if (event.target.innerText !== unescape(prevAnswer.entitled)) {
-          return { ...prevAnswer, isHeld: false };
-        }
-        return { ...prevAnswer, isHeld: true };
-      });
-    });
-  }
-
-  const answerElements = answers.map(({ entitled, isCorrect, isHeld }) => {
-    return (
-      <Answer
-        key={entitled}
-        entitled={entitled}
-        isCorrect={isCorrect}
-        isHeld={isHeld}
-        selectAnswer={selectAnswer}
-        quizSubmitted={props.isQuizSubmitted}
-      />
-    );
-  });
+  const answerElements = props.answers.map(
+    ({ entitled, isCorrect, isHeld }) => {
+      return (
+        <Answer
+          key={entitled}
+          entitled={entitled}
+          isCorrect={isCorrect}
+          isHeld={isHeld}
+          selectAnswer={props.selectAnswer}
+          quizSubmitted={props.isQuizSubmitted}
+        />
+      );
+    }
+  );
 
   const answersStyle = {
     pointerEvents: props.isQuizSubmitted ? "none" : "",
@@ -34,8 +22,8 @@ export default function Question(props) {
 
   return (
     <div className="question">
-      <div className="category">{unescape(props.category)}</div>
-      <div className="entitled">{unescape(props.entitled)}</div>
+      <div className="category">{props.category}</div>
+      <div className="entitled">{props.entitled}</div>
       <ul className="answers" style={answersStyle}>
         {answerElements}
       </ul>
